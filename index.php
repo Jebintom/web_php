9e2e463349d228f9f8594c2d38546a0a68ba9a58
Jebin Tom <95645576+Jebintom@users.noreply.github.com>
2024-09-24 09:55:35 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$name="jebin";
+$age="21";
+echo "i love web programming using PHP!<br>";
+print("<h1>hi i am $name and i am $age years old</h1>");
+echo "floor of  number is:\t";
+echo(floor(-5.3)."<br>");
+echo "random number is:\t";
+echo(rand()." <br>");
+define("MINSIZE",50);
+echo MINSIZE;
+
+?>
---
+++ inputhandling.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Enter your name and hit the submit button<br>
+    <form action="testhello.php" method="post">
+        Enter name:<input name="myname" type=text><br><br>
+        <input type="submit" name="submit" value="Greet Me">
+</form>
+</body>
+</html>
+</body>
+</html>
